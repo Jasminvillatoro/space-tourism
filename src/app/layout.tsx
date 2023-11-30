f82eb12ef0e12320540c244c329c20cb98eda95b
Jasminvillatoro <89315596+Jasminvillatoro@users.noreply.github.com>
2023-11-30 16:55:36 -0600

    imported and applied fonts to be used in project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,21 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Bellefair, Barlow_Condensed } from 'next/font/google';
 import './globals.css';
+import { Navbar } from './components/Navbar';
+import { Footer } from './components/Footer';
 
-const inter = Inter({ subsets: ['latin'] });
+const bellefair = Bellefair({
+  subsets: ['latin-ext'],
+  weight: '400',
+  variable: '--font-bellefair',
+});
+
+const barlowCondensed = Barlow_Condensed({
+  subsets: ['latin'],
+  weight: '400',
+  style: 'normal',
+  variable: '--font-barlow-condensed',
+});
 
 export const metadata: Metadata = {
   title: 'Space Tourism',
@@ -16,7 +29,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`
+        ${bellefair.variable} ${barlowCondensed.variable}`}>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
