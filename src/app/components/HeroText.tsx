a81c62389ee47cdce51ea3648106a6c767ed1404
Jasminvillatoro <89315596+Jasminvillatoro@users.noreply.github.com>
2024-04-26 21:37:37 -0500

    Hero text mobile component

---
+++ src/app/components/HeroText.tsx
@@ -0,0 +1,16 @@
+export const HeroText = () => {
+  return (
+    <div className='flex flex-col items-center max-w-[327px] mx-auto'>
+      <h2 className='font-barlow tracking-[0.144em] mt-12 mb-2 text-base font-thin'>
+        SO, YOU WANT TO TRAVEL TO
+      </h2>
+      <h1 className='text-[5rem]'>SPACE</h1>
+      <p className='font-barlow font-extralight text-[0.938rem] leading-6 text-center p-2'>
+        Let’s face it; if you want to go to space, you might as well genuinely
+        go to outer space and not hover kind of on the edge of it. Well sit
+        back, and relax because we’ll give you a truly out of this world
+        experience!
+      </p>
+    </div>
+  );
+};
