3872b28d80266e73b9f100e5a826e27ccd9ab379
Jasminvillatoro <89315596+Jasminvillatoro@users.noreply.github.com>
2024-04-26 21:41:37 -0500

    2 new home components HeroText and HomeCircle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import { Background } from './components/Background';
+import { HeroText } from './components/HeroText';
+import { HomeCircle } from './components/HomeCircle';
 export default function Home() {
   return (
     <main>
+      <HeroText />
+      <HomeCircle />
       Page
       <Background />
     </main>
