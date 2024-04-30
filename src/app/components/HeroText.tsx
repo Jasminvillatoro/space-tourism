8375af7783865564f77dcac5510a86f4c6a313b1
Jasminvillatoro <89315596+Jasminvillatoro@users.noreply.github.com>
2024-04-29 20:10:15 -0500

    hero text css edits

--- src/app/components/HeroText.tsx
+++ src/app/components/HeroText.tsx
@@ -5,7 +5,7 @@ export const HeroText = () => {
         SO, YOU WANT TO TRAVEL TO
       </h2>
       <h1 className='text-[5rem]'>SPACE</h1>
-      <p className='font-barlow font-extralight text-[0.938rem] leading-6 text-center p-2'>
+      <p className='font-barlow font-thin text-base leading-[1.6rem] text-center p-2'>
         Let’s face it; if you want to go to space, you might as well genuinely
         go to outer space and not hover kind of on the edge of it. Well sit
         back, and relax because we’ll give you a truly out of this world
