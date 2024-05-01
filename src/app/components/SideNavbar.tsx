aaa8403430617c133b5510b84c551b7e30f6fd8e
Jasminvillatoro <89315596+Jasminvillatoro@users.noreply.github.com>
2024-05-01 13:46:07 -0500

    side navbar styling improvements

--- src/app/components/SideNavbar.tsx
+++ src/app/components/SideNavbar.tsx
@@ -7,37 +7,40 @@ export const SideNavbar = () => {
     console.log('clicked');
   };
   return (
-    <div className='bg-gray-800'>
-      <Image
-        src={hamburgerClose}
-        alt='hamburger menu'
-        width={21}
-        height={24}
-        onClick={openMenu}
-        className='w-[21px] h-6'
-      />
-      <ul>
-        <li>
-          <Link href='/'>
-            <p>00 Home</p>
-          </Link>
-        </li>
-        <li>
-          <Link href='/destination'>
-            <p>01 Destination</p>
-          </Link>
-        </li>
-        <li>
-          <Link href='/crew'>
-            <p>02 Crew</p>
-          </Link>
-        </li>
-        <li>
-          <Link href='/technology'>
-            <p>03 Technology</p>
-          </Link>
-        </li>
-      </ul>
+    <div className='absolute -top-[64px] right-0'>
+      <div className='w-64 h-screen backdrop-blur-xl bg-white/10  flex flex-col'>
+        <Image
+          src={hamburgerClose}
+          alt='hamburger menu'
+          width={21}
+          height={20}
+          onClick={openMenu}
+          className='w-[21px] h-6 float-right ml-auto mr-6 mt-6'
+        />
+        {/* create array of objects later on and map through it */}
+        <ul className=' '>
+          <li>
+            <Link href='/'>
+              <p>00 Home</p>
+            </Link>
+          </li>
+          <li>
+            <Link href='/destination'>
+              <p>01 Destination</p>
+            </Link>
+          </li>
+          <li>
+            <Link href='/crew'>
+              <p>02 Crew</p>
+            </Link>
+          </li>
+          <li>
+            <Link href='/technology'>
+              <p>03 Technology</p>
+            </Link>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
