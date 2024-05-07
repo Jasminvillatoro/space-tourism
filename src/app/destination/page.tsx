cb78f3bd00a95982cd9a7d3d840e0f36e9cb6b1d
Jasminvillatoro <89315596+Jasminvillatoro@users.noreply.github.com>
2024-05-06 22:43:28 -0500

    issue fixed. Error about using fill on Image but not applying relative,fixed, or absolute to parent element

--- src/app/destination/page.tsx
+++ src/app/destination/page.tsx
@@ -1,3 +1,18 @@
+import destinationMobile from '../../../public/assets/destination/background-destination-mobile.jpg';
+import Image from 'next/image';
 export default function Destination() {
-  return <div className='text-xl text-black'>Destination</div>;
+  return (
+    <div className='text-xl text-white relative -z-10 overflow-hidden h-screen w-screen -mt-16'>
+      DESTINATION
+      <Image
+        src={destinationMobile}
+        alt='destination background'
+        placeholder='blur'
+        quality={100}
+        fill
+        sizes='100vw'
+        className='object-cover -z-10  '></Image>
+      {/* <div className='w-20 h-20 bg-white '></div> */}
+    </div>
+  );
 }
--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -3,13 +3,14 @@ import backGroundImage from '/Users/jasminvillatoro/space-tourism/public/assets/
 
 export const Background = () => {
   return (
-    <Image
-      alt='space background image'
-      src={backGroundImage}
-      placeholder='blur'
-      quality={100}
-      sizes='100vh'
-      fill
-      className=' object-cover -z-10'></Image>
+    <div className='fixed -z-10 overflow-hidden h-screen w-screen -mt-16'>
+      <Image
+        alt='space background image'
+        src={backGroundImage}
+        placeholder='blur'
+        quality={100}
+        fill
+        className='object-cover -z-10'></Image>
+    </div>
   );
 };
