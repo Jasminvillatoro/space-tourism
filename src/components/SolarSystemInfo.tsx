7b7d5aaf148aac543337d6731024d0885321b528
Jasminvillatoro <89315596+Jasminvillatoro@users.noreply.github.com>
2024-05-24 21:40:27 -0500

    condensing the space object links

--- src/components/SolarSystemInfo.tsx
+++ src/components/SolarSystemInfo.tsx
@@ -5,40 +5,33 @@ import { spaceInfo } from '../app/information/space-info';
 import Link from 'next/link';
 // import { spaceFacts } from '../app/information/space-info';
 // I'm just going to create a new page for every moon or planet I will need a dynamic route
+const spaceObjects = ['MOON', 'MARS', 'EUROPA', 'TITAN'];
 export const SolarSystemInfo = () => {
   return (
-    <div className='mt-14 flex flex-col items-center'>
-      <h2>01 PICK YOUR DESTINATION</h2>
-      <Image src={moon} alt='moon' width={170} height={170} />
-      <ul className='flex'>
-        <li>
-          <Link href='/'>
-            <p>Moon</p>
-          </Link>
-        </li>
-        <li>
-          <Link href='/'>
-            <p>Mars</p>
-          </Link>
-        </li>
-        <li>
-          <Link href='/'>
-            <p>Europa</p>
-          </Link>
-        </li>
-        <li>
-          <Link href='/'>
-            <p>Titan</p>
-          </Link>
-        </li>
+    <div className='mt-24 flex flex-col items-center relative h-full'>
+      <h2 className='font-barlow tracking-[0.169rem] text-xs  hover:bg-violet-600'>
+        <span className='opacity-25'>01 </span>PICK YOUR DESTINATION
+      </h2>
+      <Image src={moon} alt='moon' width={170} height={170} className='mt-6' />
+      <ul className='flex gap-x-4 mt-8 font-barlow tracking-[0.169rem] text-xs hover:border-b-2 hover:text-pink-500'>
+        {spaceObjects.map((item, index) => {
+          return (
+            <li key={index}>
+              <Link href='/'>
+                <p>{item}</p>
+              </Link>
+            </li>
+          );
+        })}
       </ul>
-      <h1>Moon</h1>
-      <p>
+      <h1 className='my-8 text-[56px]'>Moon</h1>
+      <p className='w-[330px] text-center text-lg mb-4 border-b-2 bg-opacity-70'>
         See our planet as you’ve never seen it before. A perfect relaxing trip
         away to help regain perspective and come back refreshed. While you’re
         there, take in some history by visiting the Luna 2 and Apollo 11 landing
         sites.
       </p>
+
       <h3>AVG. DISTANCE</h3>
       <h2>384,400 km</h2>
       <h3>Est. travel time</h3>
@@ -46,16 +39,3 @@ export const SolarSystemInfo = () => {
     </div>
   );
 };
-{
-  /* {spaceInfo.map((el, index) => (
-        <div key={el.id}>
-          <p>
-            {el.name}
-            {index}
-          </p>
-          <Image src={el.image} alt='moon' width={50} height={50} /> */
-}
-
-{
-  /* ))} */
-}
