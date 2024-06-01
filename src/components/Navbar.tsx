99700d6ab7ae791eda9c742c3fa8e91c4d8f660e
Jasminvillatoro <89315596+Jasminvillatoro@users.noreply.github.com>
2024-05-31 21:53:49 -0500

    click on logo to go to home page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,7 @@ import logo from '/Users/jasminvillatoro/space-tourism/public/assets/shared/logo
 import hamburger from '/Users/jasminvillatoro/space-tourism/public/assets/shared/icon-hamburger.svg';
 import { SideNavbar } from './SideNavbar';
 import { useState } from 'react';
+import Link from 'next/link';
 
 export const Navbar = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -15,14 +16,22 @@ export const Navbar = () => {
   return (
     <header>
       <div className='flex justify-between items-center mt-6 mx-auto max-w-[327px]'>
-        <Image src={logo} alt='logo' width={40} height={40} />
+        <Link href='/'>
+          <Image
+            src={logo}
+            alt='logo'
+            width={40}
+            height={40}
+            className='hover:cursor-pointer'
+          />
+        </Link>
         <Image
           src={hamburger}
           alt='hamburger menu'
           width={21}
           height={24}
           onClick={toggle}
-          className='w-[21px] h-6'
+          className='w-[21px] h-6 hover:cursor-pointer'
         />
       </div>
       <div className='relative'>
