108d3b0d02310ed2c45d6d08ea4ae3ce31cc66fd
Jasminvillatoro <89315596+Jasminvillatoro@users.noreply.github.com>
2024-06-05 15:03:26 -0500

    small page structure chane

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
-
 export default function NotFound() {
-  return <main>We could not find that page</main>;
+  return (
+    <main>
+      <h2>We could not find that page</h2>
+    </main>
+  );
 }
