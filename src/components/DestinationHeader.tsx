2a42687bfb99fea587301f62ba6f6d3885f414b4
Jasminvillatoro <89315596+Jasminvillatoro@users.noreply.github.com>
2024-06-10 22:35:32 -0500

    new header component

---
+++ src/components/DestinationHeader.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function DestinationHeader() {
+  return (
+    <>
+      <h2 className='font-barlow tracking-[0.169rem] text-sm'>
+        <span className='opacity-25'>01 </span>PICK YOUR DESTINATION
+      </h2>
+    </>
+  );
+}
