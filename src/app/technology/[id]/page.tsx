7097d615050928290a382d763db6d9e2df35e5c6
Jasminvillatoro <89315596+Jasminvillatoro@users.noreply.github.com>
2025-10-09 22:18:10 -0500

    Technology page. Use prisma and neondb to get technology data

--- src/app/technology/[id]/page.tsx
+++ src/app/technology/[id]/page.tsx
@@ -1,28 +1,27 @@
 import TechnologyBg from '@/components/TechnologyBg';
 import Title from '@/components/PageSubHeader';
-import TTech from '@/app/lib/types/TTech';
 import Image from 'next/image';
 import TechNav from '@/components/TechNav';
-import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
+import { prisma } from '@/lib/prisma';
 
-async function getTech(id: string) {
-  const res = await fetch(`http://localhost:4000/technology/${id}`);
-  const data = await res.json();
-  return data;
-}
-export default async function Technology(
-  props: {
-    params: Promise<{ id: string }>;
-  }
-) {
-  const params = await props.params;
-  const tech = await getTech(params.id);
+export default async function Technology({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const { id } = await params;
+  const technologyId = Number(id);
+  const technology = await prisma.technology.findUnique({
+    where: { id: technologyId },
+  });
+
+  if (!technology) return <h1>technology not found</h1>;
   return (
     <main className='text-black flex flex-col justify-center items-center pt-10 pb-12'>
       <TechnologyBg />
       <Title order='03' title='SPACE LAUNCH 101' />
       <Image
-        src={`/${tech.images.landscape}`}
+        src={`/${technology.images}`}
         alt='technology'
         width={170}
         height={170}
@@ -33,10 +32,10 @@ export default async function Technology(
         THE TERMINOLOGY…
       </h2>
       <h1 className='text-white text-2xl uppercase tracking-wide mb-3'>
-        {tech.name}
+        {technology.name}
       </h1>
       <p className='w-[327px] h-[175px] align-top  font-barlow text-center text-base bg-opacity-60 text-[#D0D6F9] leading-7 tracking-wider font-extralight'>
-        {tech.description}
+        {technology.description}
       </p>
     </main>
   );
