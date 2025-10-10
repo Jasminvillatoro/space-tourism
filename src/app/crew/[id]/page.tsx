e70f9b43f3edac07ea9d73c36c3aeabb4ba32021
Jasminvillatoro <89315596+Jasminvillatoro@users.noreply.github.com>
2025-10-09 22:17:16 -0500

    crew page. Use prisma and neondb to get crew data

--- src/app/crew/[id]/page.tsx
+++ src/app/crew/[id]/page.tsx
@@ -1,58 +1,46 @@
 import CrewBg from '@/components/CrewBg';
 import Title from '@/components/PageSubHeader';
-import type { TCrew } from '@/app/lib/types/TCrew';
 import Image from 'next/image';
 import CrewNav from '@/components/CrewNav';
-import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
-import crewData from '../../lib/crewData.json';
+import { prisma } from '@/lib/prisma';
 
-type Props = {
-  crew: TCrew;
-};
+async function CrewPage({ params }: { params: { id: string } }) {
+  const { id } = await params;
+  const crewId = Number(id);
+  const crew = await prisma.crew.findUnique({
+    where: { id: crewId },
+  });
 
-const CrewPage: NextPage<Props> = ({ crew }) => {
+  if (!crew) return <h1>Crew not found</h1>;
   return (
-    <main className='text-black flex flex-col justify-center items-center pt-10 pb-12'>
-      <CrewBg />
-      <Title order='02' title='MEET YOUR CREW' />
+    <>
+      <main className='text-black flex flex-col justify-center items-center pt-10 pb-12'>
+        <CrewBg />
+        <Title order='02' title='MEET YOUR CREW' />
 
-      <Image
-        src={`/${crew.images.png}`}
-        alt={crew.name}
-        width={170}
-        height={170}
-        className='mt-8 ml-8'
-      />
+        <Image
+          src={`/${crew.images}`}
+          alt={crew.name}
+          width={170}
+          height={170}
+          className='mt-8 ml-8'
+        />
 
-      <div className='h-[1px] w-80 bg-white/10'></div>
-      <CrewNav />
+        <div className='h-[1px] w-80 bg-white/10'></div>
+        <CrewNav />
 
-      <h2 className='mt-8 font-bellefair text-white opacity-50 text-xs text-center uppercase tracking-widest w-[108px]'>
-        {crew.role}
-      </h2>
-      <h1 className='w-[327px] text-white font-bellefair text-2xl text-center uppercase mt-2 mb-3'>
-        {crew.name}
-      </h1>
-      <p className='w-[327px] px-6 font-barlow text-center text-base bg-opacity-60 text-[#D0D6F9] leading-7 tracking-wide font-extralight pb-8'>
-        {crew.bio}
-      </p>
-    </main>
+        <h2 className='mt-8 font-bellefair text-white opacity-50 text-xs text-center uppercase tracking-widest w-[108px]'>
+          {crew.role}
+        </h2>
+        <h1 className='w-[327px] text-white font-bellefair text-2xl text-center uppercase mt-2 mb-3'>
+          {crew.name}
+        </h1>
+        <p className='w-[327px] px-6 font-barlow text-center text-base bg-opacity-60 text-[#D0D6F9] leading-7 tracking-wide font-extralight pb-8'>
+          {crew.bio}
+        </p>
+      </main>
+    </>
   );
-};
-
-export const getStaticPaths: GetStaticPaths = async () => {
-  return {
-    paths: crewData.map((c) => ({ params: { id: c.id } })),
-    fallback: false,
-  };
-};
-
-export const getStaticProps: GetStaticProps<Props> = async ({ params }) => {
-  const crew = crewData.find((c) => c.id === params?.id);
-  if (!crew) {
-    return { notFound: true };
-  }
-  return { props: { crew } };
-};
+}
 
 export default CrewPage;
