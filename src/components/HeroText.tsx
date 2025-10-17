ae6ddd4fa9ada5d4aae5d7f4704083f59346d11d
Jasminvillatoro <89315596+Jasminvillatoro@users.noreply.github.com>
2025-10-16 21:53:36 -0500

    Tablet view css

--- src/components/HeroText.tsx
+++ src/components/HeroText.tsx
@@ -4,8 +4,8 @@ export const HeroText = () => {
       <h2 className='font-barlow tracking-[0.144em] mt-12 mb-2 text-base font-thin md:text-xl md:mb-0'>
         SO, YOU WANT TO TRAVEL TO
       </h2>
-      <h1 className='text-[5rem] md:text-[150px] md:m-0 md:p-0'>SPACE</h1>
-      <p className='font-barlow font-thin text-base leading-[1.6rem] text-center p-2 text-[#d0d6f9] md:leading-7 md:w-[444px] md:h-[112px] md:-mt-8'>
+      <h1 className='text-[5rem] md:text-[150px] md:-mb-6'>SPACE</h1>
+      <p className='font-barlow font-thin text-base leading-[1.6rem] text-center p-2 text-[#d0d6f9] md:leading-7 md:w-[444px] md:h-[112px] md:p-12 md:-mt-10'>
         Let’s face it; if you want to go to space, you might as well genuinely
         go to outer space and not hover kind of on the edge of it. Well sit
         back, and relax because we’ll give you a truly out of this world
--- src/components/HomeCircle.tsx
+++ src/components/HomeCircle.tsx
@@ -1,6 +1,6 @@
 export const HomeCircle = () => {
   return (
-    <div className='w-[9.375rem] h-[9.375rem] bg-white rounded-full   text-black flex items-center justify-center mx-auto text-xl mt-14 md:w-[15.1rem] md:h-[15.1rem]'>
+    <div className='w-[9.375rem] h-[9.375rem] bg-white rounded-full text-black flex items-center justify-center mx-auto text-xl mt-14 md:text-3xl md:tracking-[2px]   md:w-[15.1rem] md:h-[15.1rem] md:mt-40'>
       EXPLORE
     </div>
   );
